8bd5fd6d8374688adfa58af9e7454863a35b1def
hyzx8 <yanzhong.han@jizhousoft.com>
2019-09-08 21:11:13 +0800

    添加密码策略

--- src/OrchardCore.Cms.Web/Startup.cs
+++ src/OrchardCore.Cms.Web/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OrchardCore.Cms.Web
@@ -7,7 +8,17 @@ namespace OrchardCore.Cms.Web
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services){
-            services.AddOrchardCms();
+            services.AddOrchardCms(); 
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredUniqueChars = 3;
+                options.Password.RequiredLength = 6;
+
+            });
             //配置跨域处理，允许所有来源：
             services.AddCors(options =>
             options.AddPolicy("JZSoftCros",
