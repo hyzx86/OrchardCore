8fbea212cb0b83b0fdcc082a05b691791ca2a300
Tony Han <hyzx86@live.com>
2019-12-27 13:54:13 +0800

    add cros

--- src/OrchardCore.Cms.Web/Startup.cs
+++ src/OrchardCore.Cms.Web/Startup.cs
@@ -7,8 +7,9 @@ namespace OrchardCore.Cms.Web
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services){
-            services.AddOrchardCms(); 
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOrchardCms();
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = false;
@@ -21,32 +22,15 @@ namespace OrchardCore.Cms.Web
             });
             //配置跨域处理，允许所有来源：
             services.AddCors(options =>
-            options.AddPolicy("JZSoftCros",
-            p => p.AllowAnyOrigin())
-            );
-
-            #region 跨域
-            //允许一个或多个具体来源:
-            //services.AddCors(options =>
-            //{
+    options.AddPolicy("cors",
+    p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
 
 
-            //    Policy 名稱 CorsPolicy 是自訂的，可以自己改
-            //    options.AddPolicy("跨域规则的名称", policy =>
-            //    {
-            //        // 設定允許跨域的來源，有多個的話可以用 `,` 隔開
-            //        policy.WithOrigins("http://localhost:52652", "http://127.0.0.1")
-            //                .AllowAnyHeader()
-            //                .AllowAnyMethod()
-            //                .AllowCredentials();
-            //    });
-            //});
-            #endregion
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseCors("JZSoftCros");
+            app.UseCors("cors");
 
             if (env.IsDevelopment())
             {
