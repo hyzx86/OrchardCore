5ec0a828b3ae0eb3ba9f35220da29448578d64db
Tony Han <hyzx86@live.com>
2019-12-31 21:17:32 +0800

    Cros Policy

--- src/OrchardCore.Cms.Web/Startup.cs
+++ src/OrchardCore.Cms.Web/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,20 +17,21 @@ namespace OrchardCore.Cms.Web
                 options.Password.RequireUppercase = false;
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequiredUniqueChars = 3;
-                options.Password.RequiredLength = 6;
-
+                options.Password.RequiredLength = 6; 
             });
             //配置跨域处理，允许所有来源：
             services.AddCors(options =>
-    options.AddPolicy("cors",
-    p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
-
-
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            app.UseCors("cors");
+            app.UseCors("CorsPolicy");
 
             if (env.IsDevelopment())
             {
